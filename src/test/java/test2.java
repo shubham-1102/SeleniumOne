3b942510dd1b8fec42c9bcb338d09f5990c7d9a5
shubham-1102 <shubhampatil1102@gmail.com>
2023-09-22 12:28:12 +0530

    c1

---
+++ src/test/java/test2.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/Register.html");
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector("css=font:contains(“Choose File”)")).click();
+	}
+
+}
